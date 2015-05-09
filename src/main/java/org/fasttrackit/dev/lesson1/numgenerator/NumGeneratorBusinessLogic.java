cf0401eebe4210c6e57f8c932fff73bb07b556ab
Ionel Condor <ionel-condor@yahoo.com>
2015-05-09 18:05:04 +0300

    10

--- src/main/java/org/fasttrackit/dev/lesson1/numgenerator/NumGeneratorBusinessLogic.java
+++ src/main/java/org/fasttrackit/dev/lesson1/numgenerator/NumGeneratorBusinessLogic.java
@@ -16,7 +16,7 @@ FastTrackIT 2015
 
 public class NumGeneratorBusinessLogic {
 
-    private static final int MAXNUMBER = 10;
+    private static final int MAX_NUMBER = 10;
 
     private boolean isFirstTime = true;
     private boolean successfulGuess;
@@ -52,7 +52,7 @@ public class NumGeneratorBusinessLogic {
 
     public boolean determineGuess(int guessNumber){
         if (isFirstTime) {
-            generatedNumber = NumGenerator.generate(MAXNUMBER);
+            generatedNumber = NumGenerator.generate(MAX_NUMBER);
             System.out.println("gennr:"+generatedNumber);
             isFirstTime = false;
         }
